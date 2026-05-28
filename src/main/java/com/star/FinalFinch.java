99d19c1371d3185ff5b854e2663c89ff25ac8885
pinidixx <pinidie4@gmail.com>
2026-05-28 11:31:28 -0700

    update

---
+++ src/main/java/com/star/FinalFinch.java
@@ -0,0 +1,9 @@
+package com.star;
+
+import com.birdbrain.Finch;
+
+public class FinalFinch extends Finch {
+    public FinalFinch(String device) {
+        super(device);
+
+}
---
+++ src/main/java/com/star/TesterFinch.java
@@ -0,0 +1,9 @@
+package com.star;
+
+public class TesterFinch {
+    public static void main(String[] args)
+    FinalFinch bird = new FinalFinch("A");
+
+bird.setMove("F", 10, 50);
+}
+}
