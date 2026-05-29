32721bd7b9928ad2ad1344a4e86283bb358f9332
pinidixx <pinidie4@gmail.com>
2026-05-29 11:44:36 -0700

    u[date

--- src/main/java/com/star/TesterFinch.java
+++ src/main/java/com/star/TesterFinch.java
@@ -4,6 +4,21 @@ public class TesterFinch {
     public static void main(String[] args) {
     FinalFinch bird = new FinalFinch("A");
 
-bird.setMove("F", 10, 50);
+    int Speed = 150;
+    int Side = 1200;
+    int SharpTurn = 850;
+    int WideTurn = 450;
+
+    // birdcl.setWheelVelocities (Speed, Speed, Side);
+
+    // bird.setWheelVelocities (Speed, -Speed, SharpTurn);
+
+bird.setMove("F", 15, 50);
+bird.setTurn ("R", 120, 50);
+bird.setMove("F", 20, 50);
+bird.setTurn ("R", 75, 50);
+bird.setMove("F", 15, 50);
+bird.setTurn ("R", 100, 50);
+bird.setMove("F", 15, 50);
 }
 }
